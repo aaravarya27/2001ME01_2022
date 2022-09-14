3b837c7dac2ea41d674d5e38a89a140a81246c41
Aarav Arya <aarav.arya2307@gmail.com>
2022-09-14 16:34:04 +0530

    tut01 - calculated avg values of U,V,W

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,6 +1,23 @@
-def octact_identification(mod=5000):
-###Code
+import os
+import pandas as pd
+os.system('cls')
 
+def octact_identification(mod=5000):
+    # read input CSV file
+    csv1 = pd.read_csv('octant_input.csv')
+    
+    # insert U Avg, V Avg & W Avg columns with blank values
+    csv1.insert(4, 'U Avg', "", True)
+    csv1.insert(5, 'V Avg', "", True)
+    csv1.insert(6, 'W Avg', "", True)
+
+    # calculate mean values of U,V,W and assigned to index 0 row of U Avg, V Avg, W Avg
+    csv1['U Avg'][0] = csv1['U'].mean()
+    csv1['V Avg'][0] = csv1['V'].mean()
+    csv1['W Avg'][0] = csv1['W'].mean()
+
+    # display contents of CSV file upto index 4
+    print(csv1.head())
 
 from platform import python_version
 ver = python_version()
