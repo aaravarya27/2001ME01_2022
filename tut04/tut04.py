0b6b51324ddc19f1e4c5671bab39185d99077bc9
Aarav Arya <aarav.arya2307@gmail.com>
2022-10-16 23:06:29 +0530

    created columns 15,16,17 and 18

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -30,7 +30,7 @@ def octant_assign(x,y,z):
 
 def subsequence_octant(data_xlsx, len_data_xlsx, octant):
     try:
-         
+       
         count = 1 #initial value of count is 1 since atleast 1 occurance of an octant will be present  i.e. ith element
         for i in range(len_data_xlsx): #iterating over entire data set
             if(data_xlsx.iloc[i,10] == data_xlsx.iloc[i+1,10]): #if two successive octant values are same
@@ -39,19 +39,18 @@ def subsequence_octant(data_xlsx, len_data_xlsx, octant):
             else:
                 num = data_xlsx.iloc[i,10]
                 index = octant.index(num) #index of corrensponding octant number in the list octant [1,-1,2,-2,3,-3,4,-4]
-                
+               
 
                 if(count == data_xlsx.iloc[index,13] ): #if longest subsequent count remains same
                     data_xlsx.iloc[index,14] += 1 #increment count of the longest subsequent count i.e. column 14 of corresponding octant number
-                   
-
+                  
                 elif (count > data_xlsx.iloc[index,13]): #if longest subsequent count is greater than the previous one
                     data_xlsx.iloc[index,13] = count #new value of longest subsequent count in column 13 of corresponding octant number
                     data_xlsx.iloc[index,14] = 1 #reset count to 1 in coloum 14 of corresponding octant number
-                    
+                   
 
                 count = 1
-                
+    
 
     #except block in case an error occurs anywhere in the above function
     except:
@@ -104,7 +103,10 @@ def octant_longest_subsequence_count():
         data_xlsx.insert(13, 'Longest Subsequent Length', "", True)
         # insert column 14 with Count
         data_xlsx.insert(14, 'Count', "", True)
-       
+        data_xlsx.insert(15, '', "", True)
+        data_xlsx.insert(16, 'Octant Number', "", True)
+        data_xlsx.insert(17, 'Longest Subsequent Length', "", True)
+        data_xlsx.insert(18, 'Count', "", True)
             
         #list containing octant numbers
         octant = [1, -1, 2, -2, 3, -3, 4, -4]
@@ -130,7 +132,6 @@ def octant_longest_subsequence_count():
               
 
 
-    
 # check python version
 from platform import python_version
 ver = python_version()
@@ -153,8 +154,6 @@ except:
 
 #list containing octant numbers
 octant = [1, -1, 2, -2, 3, -3, 4, -4]
-#empty list to store start and end times for each octant number
-subsequenceTime = [[],[],[],[],[],[],[],[],[]]
 
 
 # Write over corresponding output file
