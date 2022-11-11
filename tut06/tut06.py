30c0d255f3e0aba321dac6506ab9ac6637aca898
Aarav Arya <aarav.arya2307@gmail.com>
2022-11-11 22:24:48 +0530

    read input csv files

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,11 +1,27 @@
-
-
+import os
+import pandas as pd
 from datetime import datetime
+os.system('cls')
+
 start_time = datetime.now()
 
 def attendance_report():
-###Code
 
+    try:
+        regStd = pd.read_csv("input_registered_students.csv")
+    except:
+        print('Error in reading csv file : input_registered_students')
+        exit()
+
+    try:
+        att = pd.read_csv("input_attendance.csv")
+    except:
+        print('Error in reading csv file : input_attendance')
+        exit()
+
+    
+
+# check python version
 from platform import python_version
 ver = python_version()
 
@@ -14,11 +30,7 @@ if ver == "3.8.10":
 else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
-
 attendance_report()
-
-
-
 
 #This shall be the last lines of the code.
 end_time = datetime.now()
