1cc55f919c424cbe0477b001c7b80f5aaef3c3e0
Aarav Arya <aarav.arya2307@gmail.com>
2022-11-20 10:50:47 +0530

    read input files and converted all entries of attendance in upper case

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,27 +1,26 @@
 import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 os.system('cls')
 
 start_time = datetime.now()
 
 def attendance_report():
 
-    try:
-        regStd = pd.read_csv("input_registered_students.csv")
-    except:
-        print('Error in reading csv file : input_registered_students')
-        exit()
+    # read input files as csv
+    input_attendance = "./input_attendance.csv"
+    input_registered_students = "./input_registered_students.csv"
 
-    try:
-        att = pd.read_csv("input_attendance.csv")
-    except:
-        print('Error in reading csv file : input_attendance')
-        exit()
+    att = pd.read_csv(input_attendance)
+    reg = pd.read_csv(input_registered_students)
 
+    # converted all entries in attendance file to upper case
+    att.dropna(axis="index", inplace = True)
+    att.iloc[:, 1] = att.iloc[:, 1].apply(str.upper)
+
+    # list with each object as roll no. name
     
-
-# check python version
+#check python version
 from platform import python_version
 ver = python_version()
 
@@ -30,7 +29,7 @@ if ver == "3.8.10":
 else:
     print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
-attendance_report()
+attendance_report() #function call
 
 #This shall be the last lines of the code.
 end_time = datetime.now()
