2cf2705c1a405e92979dc2d6feafe9fd8b71bb12
Aarav Arya <aarav.arya2307@gmail.com>
2022-11-20 10:52:00 +0530

    created a list of all valid lecture dates

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -19,7 +19,38 @@ def attendance_report():
     att.iloc[:, 1] = att.iloc[:, 1].apply(str.upper)
 
     # list with each object as roll no. name
+    temp = []
+    for i in range(len(reg)):
+        temp.append(reg.iloc[i, 0] + " " + reg.iloc[i, 1])
+    reg = temp
+
+    # finding the start date 
+    for i in att["Timestamp"]:
+        valid = datetime.strptime(i, "%d-%m-%Y %H:%M")
+        # break at 1st valid date
+        if(valid.weekday() == 0 or valid.weekday() == 3): 
+            start = valid.date()
+            break
+    # finding the end date
+    for i in att["Timestamp"]:
+        valid = datetime.strptime(i, "%d-%m-%Y %H:%M")
+        if(valid.weekday() == 0 or valid.weekday() == 3):
+            end = valid.date()     
     
+    # adding all valid lecture dates to a list
+    lectures = []
+    x = start
+    while(x <= end):
+        if x.weekday() == 0 or x.weekday() == 3:
+            rev = '-'.join(str(x).split('-')[::-1]) #reverse the format of the date to get DD-MM-YYY
+            lectures.append(rev)
+        x += timedelta(days = 1) #used to increment days
+
+    # lectures.remove('15-08-2022') # 15th August if marked as holiday
+
+    
+   
+
 #check python version
 from platform import python_version
 ver = python_version()
