bcdc8bc63c59abf181591421cdf993c0a1d1391b
Aarav Arya <aarav.arya2307@gmail.com>
2022-11-20 20:04:00 +0530

    read and extracted player lists from teams file

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,14 +1,59 @@
 import os
 import pandas as pd
-from os.path import dirname, abspath
 import re
-from glob import glob
 from datetime import datetime
 os.system('cls')
 
 start_time = datetime.now()
 
+def scorecard():
+    try:
+        try:
+            #create blank output file
+            outputfile = 'Scorecard.txt'
+            f = open(outputfile, "w")
+            f.write("")
+            f.close()
+        except FileNotFoundError:
+            print('Input file not found :', outputfile)
+            exit()
+        except:
+            print('Error in file: Could not open ', outputfile)
+            exit()
 
+        try:
+            # open and read teams file to extract player list
+            teams = open('teams.txt', 'r')
+            players = []
+            for i in teams.readlines():
+                    i = i.strip()
+                    if len(i) > 0:
+                        players.append(i)
+
+            teams.close()
+        except FileNotFoundError:
+            print('Input file not found :', teams)
+            exit()
+        except:
+            print('Error in file: Could not open ', teams)
+            exit()
+
+        team1, team2 = players[0], players[1]
+        
+        #player list of team 1 and team 2
+        players1 = []
+        for i in team1.split(":")[1].split(','):
+            players1.append(i.strip())
+
+        players2 = []
+        for i in team2.split(":")[1].split(','):
+            players2.append(i.strip())
+
+        
+
+    except:
+        print('Error in function: scorecard')
+        exit()
 
 from platform import python_version
 ver = python_version()
@@ -18,13 +63,7 @@ if ver == "3.8.10":
 else:
 	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
-
 scorecard()
-
-
-
-
-
 
 #This shall be the last lines of the code.
 end_time = datetime.now()
